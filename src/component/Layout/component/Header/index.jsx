889d1b18a953449781a5ba25a981d86680f5d931
HuyDinhUI <146050567+HuyDinhUI@users.noreply.github.com>
2025-03-02 21:51:12 +0700

    test

--- src/component/Layout/component/Header/index.jsx
+++ src/component/Layout/component/Header/index.jsx
@@ -1,9 +1,31 @@
-import styles from './header.module.scss'
+import { Link } from "react-router-dom";
+import styles from "./header.module.scss";
+import FontAwesomeIcon from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 
-const Header = () =>{
-    return (
-        <h1>Header</h1>
-    )
-}
+const Header = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <h1 className={styles.title}>MyClothes</h1>
+        <div className={styles.navbar}>
+          <div className={styles["navbar-container"]}>
+            <Link className={styles["nav-link"]}>HOME</Link>
+            <Link className={styles["nav-link"]}>SHOP</Link>
+            <Link className={styles["nav-link"]}>ABOUT</Link>
+            <Link className={styles["nav-link"]}>DISCOVER</Link>
+            <Link className={styles["nav-link"]}>SOCIAL</Link>
+            <Link className={styles["nav-link"]}>OUTFIT</Link>
+          </div>
+        </div>
+        <div className={styles.actions}>
+          <div className={styles["actions-item"]}>
+            <FontAwesomeIcon icon={faUser}></FontAwesomeIcon>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
